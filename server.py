d554deb13a46513b4c1496d672467259bb43ffd1
Futaba-Kosuke <k.futaba2828@gmail.com>
2021-04-06 17:02:18 +0900

    Add: LINEボットとしての動作を実装 (#2)

---
+++ server.py
@@ -0,0 +1,49 @@
+import os
+
+from flask import Flask, request, abort
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+from utils.dice.main import exec_command
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_messaging_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route('/callback', methods=['POST'])
+def callback ():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text
+    command = text.split()[0]
+
+    result = exec_command(command)
+    line_messaging_api.reply_message(event.reply_token, TextSendMessage(text=result))
+
+def main():
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
+if __name__ == "__main__":
+    main()
